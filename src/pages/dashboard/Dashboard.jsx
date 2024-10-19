0332d33e8712ac3e989e1f088fb24f1536eb7cda
Rezwanul777 <rezwanul.haquebd777@gmail.com>
2024-10-19 23:44:08 +0600

    chat box and dash board complete

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,24 +1,34 @@
 import { CardItems } from "../../common/db"
-
+import Search from "../../components/Search"
+import Card from "./Card"
+import { IoClose } from "react-icons/io5";
+import ChatBox from "./ChatBox";
+import { useState } from "react";
 
 const Dashboard = () => {
+    const [chat, setChat] = useState(false);
+  const handleChat = () => {
+    setChat(!chat);
+  };
   return (
    <>
-
-<div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-2 p-4">
+<Search welcome="welcome back" heading="Dashboard" button="Cypher AI" />
+<section className="pb-24">
+<div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-2 p-10">
         {CardItems.map((cardDetails, i) => (
           <Card key={i} {...cardDetails} />
         ))}
       </div>
 
       <button
-        className="bg-orange text-white md:w-[77px] w-[57px] md:h-[77px] h-[57px] rounded-full inline-flex justify-center items-center bottom-4 right-4 fixed hover:cursor-pointer font-bold text-[55px] font-white font-babesNeue z-[9999] hover:scale-95 transition-all duration-300"
+        className="bg-orange text-white md:w-[77px] w-[57px] md:h-[77px] h-[57px] rounded-full inline-flex justify-center items-center bottom-2 right-4 fixed hover:cursor-pointer font-bold text-[55px] font-white font-babesNeue z-[9999] hover:scale-95 transition-all duration-500"
         onClick={handleChat}
       >
-        {chatOpen ? <IoClose /> : "C"}
+        {chat ? <IoClose /> : "C"}
       </button>
-      {/* chatBox component */}
-      <div className="fixed top-0 right-4">{chatOpen ? <ChatBox /> : null}</div>
+      {/* chatBox  */}
+      <div className="fixed  right-0 bottom-[80px]">{chat ? <ChatBox /> : null}</div>
+</section>
    
    </>
   )
