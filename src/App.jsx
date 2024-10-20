80af33a09d1ddc1fe074653c1853773ed6f3218b
Rezwanul777 <rezwanul.haquebd777@gmail.com>
2024-10-20 14:38:12 +0600

    loading add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import './App.css'
 import Navbar from './shared/Navbar'
 
 function App() {
-  
+  const navigation=useNavigation()
+
+  if(navigation.state==="loading"){
+    return <h1 className='text-center mx-auto text-orange '>Loading...</h1>
+  }
 
   return (
     <>
